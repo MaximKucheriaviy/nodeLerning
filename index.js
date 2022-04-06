68f5a1ec09b71448ce359af2f10d6821f50a2612
Maxim Kucheriaviy <maxim-k@i.ua>
2022-04-06 08:51:22 +0300

    some comit

--- index.js
+++ index.js
@@ -5,43 +5,33 @@ let counter = 0;
 function generateHtml(){
     let data = fs.readFileSync("./resourses/sourse.html", "utf-8");
     data = data.toString();
-    let starIndex = -1;
-    let ecndIndex = -1;
+    let starIndex = 0;
+    let ecndIndex = 0;
     while(true){
-        starIndex = data.indexOf("<include>", ecndIndex + 1);
+        starIndex = data.indexOf("<include>", 0);
         if(starIndex == -1){
             break;
         }
-        ecndIndex = data.indexOf("</include>", starIndex + 1);
+        ecndIndex = data.indexOf("</include>", 0);
         starIndex = starIndex + "<include>".length;
         let adress = data.slice(starIndex, ecndIndex);
         let inFile = fs.readFileSync(adress, "utf-8");
         data = data.slice(0, starIndex - "<include>".length) + inFile + data.slice(ecndIndex + "</include>".length);
-        fs.writeFileSync("./index.html", data);
     } 
-}
+    fs.writeFileSync("./index.html", data);
+    console.log("output file updated");
 
+}
+console.log("SHL startiong...");
 
 generateHtml();
-/*let data = fs.readFileSync("./resourses/index.html", "utf-8");
-data = data.toString();
 
-let starIndex = data.indexOf("<include>", 0);
-let ecndIndex = data.indexOf("</include>", starIndex);
+console.log("Start watching...");
 
-starIndex = starIndex + "<include>".length;
 
-let adress = data.slice(starIndex, ecndIndex);
-
-let inFile = fs.readFileSync(adress, "utf-8");
-
-data = data.slice(0, starIndex - "<include>".length) + inFile + data.slice(ecndIndex + "</include>".length);
-
-fs.writeFileSync("./index.html", data);*/
-
-/*console.log("Start watching...");
 watch(["./resourses", "./resourses/parshals"], {recurseve: true}, (event, filename) =>{
     if(filename){
         console.log("File changed:", filename, " ", event);
+        generateHtml();
     }
-})*/
+})
