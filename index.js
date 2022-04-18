f8456972827ebbd97d6333e92d9e2651fdc765cc
Maxim Kucheriaviy <maxim-k@i.ua>
2022-04-18 22:52:33 +0300

    server is working

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const http = require('http');
 const fs = require('file-system');
-const ropt = 2500;
+const path = require('path');
+const mime = require('mime-types');
+
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log("server reqest");
-    console.log(req.url, req.method);
-
-    let file = fs.readFileSync("./index.html", "utf-8");
-
-    res.setHeader('Content-Type', 'text/ht');
-    res.write('Hello world');
-    res.end();
+    let filePath;
+    if(req.method == "GET"){
+        if(req.url == '/'){
+            filePath = path.join('./index.html');
+        }
+        else{
+            filePath = (`.${req.url}`);
+        }
+        fs.readFile(filePath, (err, data)=>{
+            if(err){
+                console.log(err);
+                res.end();
+            }
+            else{
+                res.setHeader('Content-Type', mime.lookup(path.parse(filePath).ext));
+                res.write(data);
+                res.end();
+            }
+        })
+    }
 })
 
-server.listen(3000, 'localhost', (error) =>{
+server.listen(port, 'localhost', (error) =>{
     if(error){
         throw(error);
     }
     else{
-        console.log("Seerver run on port 3000")
+        console.log(`Seerver run on port ${port}`)
+        console.log("connect to it ad " + `http://localhost:${port}/`)
     }
 })
