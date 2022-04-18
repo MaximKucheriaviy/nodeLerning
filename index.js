08ac43c5cea45ca14bad7cb4fc346dfd120efd69
Maxim Kucheriaviy <maxim-k@i.ua>
2022-04-18 10:23:10 +0300

    server is runing

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("node works");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log("server reqest");
+    console.log(req.url, req.method);
+    
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello world');
+    res.end();
+})
+
+server.listen(3000, 'localhost', (error) =>{
+    if(error){
+        throw(error);
+    }
+    else{
+        console.log("Seerver run on port 3000")
+    }
+})
