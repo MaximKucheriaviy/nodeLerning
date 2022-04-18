849f0facba739b8512d33bfeec7a5819d8046f48
Maxim Kucheriaviy <maxim-k@i.ua>
2022-04-18 10:55:00 +0300

    comint

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http');
+const fs = require('file-system');
 const ropt = 2500;
 
 const server = http.createServer((req, res) => {
     console.log("server reqest");
     console.log(req.url, req.method);
 
-    
+    let file = fs.
 
     res.setHeader('Content-Type', 'text/ht');
     res.write('Hello world');
